18c9b2fa07d7439d670fe137816ca72aa27c0126
saadk555 <s2ksaadkhan@gmail.com>
2022-12-23 23:05:37 +0500

    added files

---
+++ README.md
@@ -0,0 +1,44 @@
+CV-MAN
+--------
+
+#  Flask-based version of CV-MAN which can be deployed over web
+
+
+- Upload all the resumes and enter amount of words you'd like to find
+
+- Enter words one by one on next page
+
+- wait for the result page download your result in zip
+
+
+For more information, please refer back to the MASTER branch's readme file
+
+
+----------------------------------------------------
+External Libraries used in this project 
+
+
+pytesseract 0.3.10
+`pip install pytesseract`
+
+
+pdf2image 1.16.0
+`pip install pdf2image`
+
+
+Pillow 9.2.0
+`pip install Pillow`
+
+Libraries and their uses are covered by their respective licenses in [Libraries](https://github.com/saadk555/cv-man/tree/master/Libraries) directory 
+
+----------------------------------------------------
+
+Feel free to contribute and improve it.
+
+Any contribution would be highly appreciated.
+
+----------------------------------------------------
+
+
+
+
---
+++ app.py
@@ -0,0 +1,123 @@
+import io
+import os
+import sys
+from ast import dump
+from glob import glob
+from io import BytesIO
+from zipfile import ZipFile
+
+import flask
+import flask_login
+from flask import *
+from flask import after_this_request, session
+from flask_login import *
+
+secret = os.environ.get("PWDS")
+
+users = {'panda@rabbit.com': {'password': 'pandarabbit37424562'}}
+
+
+
+UPLOAD_FOLDER = "/home/bluxbeeg/project.pekisa.com/UPLOAD_FOLDER/"
+ALLOWED_EXTENSIONS = 'pdf'
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000
+app.secret_key = os.environ.get("SK")
+app.static_folder = 'static'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+keyword = []
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if email in users and flask.request.form['password'] == users[email]['password']:
+        user = User() 
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return "Sorry, login failed :("
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'You have been succcessfully logged out'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'You are logged in as: ' + flask_login.current_user.id
+
+@app.route('/')  
+@login_required
+def index(): 
+    return render_template("index.html")  
+
+@app.route('/success', methods = ['POST'])  
+@login_required
+def success():  
+    if request.method == 'POST':  
+        key = request.form['k']  
+        return render_template("keywords.html", number = int(key))  
+
+
+
+@app.route('/submitted', methods = ['POST'])
+@login_required
+def submitted():
+    if request.method == 'POST':
+        return render_template("result.html")
+        
+        
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized, Please visit https://project.pekisa.com/login'
+
+    
+
+if __name__ == '__main__':  
+    app.run(host= '0.0.0.0', debug = True) 
